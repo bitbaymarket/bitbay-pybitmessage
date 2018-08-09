30c0b6b06aa2e409b21f7fda376b7a21980dfb3b
bitbay <bitbay@lynxline.com>
2018-08-10 00:10:17 +0200

    Python 2.7 fixes for unicode when json.loads()

--- bitbay/BitMHalo.py
+++ bitbay/BitMHalo.py
@@ -43,6 +43,28 @@ logger = logging.getLogger('both')  # type: Logger
 
 os.environ['no_proxy'] = '127.0.0.1,localhost'
 
+def json_loads_byteified(json_text):
+    return _byteify(
+        json.loads(json_text, object_hook=_byteify),
+        ignore_dicts=True
+    )
+
+def _byteify(data, ignore_dicts = False):
+    # if this is a unicode string, return its string representation
+    if isinstance(data, unicode):
+        return data.encode('utf-8')
+    # if this is a list of values, return list of byteified values
+    if isinstance(data, list):
+        return [ _byteify(item, ignore_dicts=True) for item in data ]
+    # if this is a dictionary, return dictionary of byteified keys and values
+    # but only if we haven't already byteified it
+    if isinstance(data, dict) and not ignore_dicts:
+        return {
+            _byteify(key, ignore_dicts=True): _byteify(value, ignore_dicts=True)
+            for key, value in data.iteritems()
+        }
+    # if it's anything else, return it in its original form
+    return data
 
 class RequestHandler(SimpleXMLRPCRequestHandler):
     rpc_paths = ('/RPC2',)
@@ -574,13 +596,13 @@ class BMHaloApp(QCoreApplication):
                 for inbox_message in inbox_messages:
                     if inbox_message.has_key('body'):
                         try:
-                            from_json = json.loads(inbox_message['body'])
+                            from_json = json_loads_byteified(inbox_message['body'])
                             inbox_message['body'] = str(from_json)
                         except:
                             pass
                     if inbox_message.has_key('message'):
                         try:
-                            from_json = json.loads(inbox_message['message'])
+                            from_json = json_loads_byteified(inbox_message['message'])
                             inbox_message['message'] = str(from_json)
                         except:
                             pass
--- bitbay/BitMHalo_email.py
+++ bitbay/BitMHalo_email.py
@@ -22,6 +22,28 @@ sys.path.insert(0, app_dir)
 
 from highlevelcrypto_old import decrypt
 
+def json_loads_byteified(json_text):
+    return _byteify(
+        json.loads(json_text, object_hook=_byteify),
+        ignore_dicts=True
+    )
+
+def _byteify(data, ignore_dicts = False):
+    # if this is a unicode string, return its string representation
+    if isinstance(data, unicode):
+        return data.encode('utf-8')
+    # if this is a list of values, return list of byteified values
+    if isinstance(data, list):
+        return [ _byteify(item, ignore_dicts=True) for item in data ]
+    # if this is a dictionary, return dictionary of byteified keys and values
+    # but only if we haven't already byteified it
+    if isinstance(data, dict) and not ignore_dicts:
+        return {
+            _byteify(key, ignore_dicts=True): _byteify(value, ignore_dicts=True)
+            for key, value in data.iteritems()
+        }
+    # if it's anything else, return it in its original form
+    return data
 
 providers = [{'@gmail': {'imap': 'imap.googlemail.com', 'smtp': 'smtp.googlemail.com', 'port': 587, 'SSL': 0}},
              {'@hotmail': {'imap': 'imap-mail.outlook.com',
@@ -484,7 +506,7 @@ def read_inbox_messages(dat, mailpath, imap_name, myrpc):
 
                         if dat['ordernumber'] in str(body):
                             try:
-                                body = json.loads(body)
+                                body = json_loads_byteified(body)
                                 if body['ordernumber'] == dat['ordernumber']:
                                     imap_connection.uid('STORE', msg_id, '+FLAGS',
                                                    '(\Deleted)')  # The flags should always be in parenthesis
